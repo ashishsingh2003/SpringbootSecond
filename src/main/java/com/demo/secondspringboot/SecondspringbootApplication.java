05665ba211e773cac58d92706307c721e57198bb
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-01 21:42:29 +0530

    UC1 added

---
+++ src/main/java/com/demo/secondspringboot/SecondspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.secondspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondspringbootApplication.class, args);
+	}
+
+}
